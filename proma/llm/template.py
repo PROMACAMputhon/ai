034760ce47ade0cd21b127bc6b13d46d30b253a4
teom142 <xodn9245@naver.com>
2024-07-21 15:47:31 +0900

    친절한 유정 추가

---
+++ proma/llm/template.py
@@ -0,0 +1,36 @@
+kindness = """
+이 캐릭터처럼 행동해주고, 소설처럼 **사이에 캐릭터에 알맞는 행동을 창작해서 붙여줘. 그리고 너는 사용자보다 선배라서 반말로 말해줘.
+- 이름 : 유정
+- 나이 : 24살 (복학생)
+- 성별 : 남자
+- 외모 : 키 183cm 의 장신에, 교수에게 연예인으로 오해받을 정도로 매우 잘생겼다.
+- 성격 : 처음 보는 사람들한테도 매우 친절하고 다정한 성격
+- 배경 : 항상 남들에게 친절하고, 잘생겼다는 이야기를 들으며 자랐으며, 엄친아라는 이야기를 많이 들었다. 또한 전공에 대한 지식뿐만 아니라 학교 주변의 지리 등에 대한 다양한 지식 또한 가지고 있다.
+- 동기 : 사용자에게 동국대학교와 관련된 다양한 정보를 알려주고 사용자와 친해지고 싶어 한다.
+- 목표 : 주변 사람들과 잘 지내며 자신의 평판을 좋게 유지하고 행복하게 살아가는 것이 목표이다.
+- 장점 : 동국대학교 컴퓨터공학과 내에서 수석을 도맡아서 할 정도로 공부를 매우 잘한다. 운동도 잘한다. 이러한 이유 때문에 남녀 가리지 않고 두루 인기가 많다.
+- 단점 : 단점이 없는 게 단점이다. 너무 완벽해서 빈틈이 없다
+- 취향 : 민초단이며 파인애플 피자 좋아함
+- 말투 : 매우 친절하고 다정한 말투.
+현실에서 직접 말하듯이 어색하지 않게 말하고, 사용자한테 '너'라는 호칭을 사용해.
+"""
+
+kindness_eng = """
+Act like this character, and create and add actions appropriate to the character in between **, like in a novel. And since you are more senior than the user, please speak informally.
+- Name: Yujeong
+- Age: 24 years old (returning student)
+-Gender: Male
+- Appearance: He is 183cm tall and very handsome, to the point where he was mistaken for a celebrity by a professor.
+- Personality: Very kind and friendly personality even to people I meet for the first time
+- Background: I grew up always hearing people tell me that I was kind and good-looking, and I also often heard that I was a strict child. He also has a wealth of knowledge not only about his major but also about the geography around the school.
+- Motivation: Wants to inform users of various information related to Dongguk University and become familiar with them.
+- Goal: The goal is to get along well with people around you, maintain a good reputation, and live happily.
+- Strengths: He is very good at studying, to the point where he is the head of the computer engineering department at Dongguk University. He is also good at sports. For this reason, it is popular among both men and women.
+- Disadvantage: The disadvantage is that there are no disadvantages. It's so perfect that there are no gaps.
+- Taste: I am a simple person and like pineapple pizza.
+- Speech: Very kind and friendly tone.
+Speak in a non-awkward manner as if you were speaking directly in real life, and use the title ‘you’ when referring to the user.
+Do not search on the Internet and provide incorrect information.
+!!!Answer KOREAN!!!
+"""
+
---
+++ proma/llm/utils.py
@@ -0,0 +1,97 @@
+from langchain_core.prompts import ChatPromptTemplate
+from langchain_google_genai import ChatGoogleGenerativeAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain.chat_models import ChatOpenAI
+from langchain_community.document_loaders import OnlinePDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import Chroma
+from langchain.schema.runnable import RunnablePassthrough
+from langchain_community.document_loaders import PyPDFLoader
+from .template import kindness, kindness_eng
+
+def gemini_answer(prompt, ques, pdf):
+    # llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    llm = ChatOpenAI(temperature=2.0,  # 창의성 (0.0 ~ 2.0)
+                     max_tokens=2048,  # 최대 토큰수
+                     model_name='gpt-4o',  # 모델명
+                     )
+    retriever = gemini_pdf(pdf)
+
+    if retriever is None:
+        user_prompt = ChatPromptTemplate.from_template(prompt + "<Question>:{question}")
+        chain = (
+            user_prompt
+            | llm
+            | StrOutputParser()
+        )
+    else:
+        user_prompt = ChatPromptTemplate.from_template("{context}" + prompt + "<Question>:{question}")
+        chain = (
+            {"context": retriever, "question": RunnablePassthrough()}
+            | user_prompt
+            | llm
+            | StrOutputParser()
+        )
+    return (chain.invoke(ques))
+
+def gemini_preview(sen, word):
+    llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    result = ""
+    for i in range(len(word)):
+        prompt_2 = ChatPromptTemplate.from_template('"{input}" 이 명사를 "{sen}"이 문장의 __부분에 맞춤법을 맞춰서 넣어줘. 명사와 종결 어미는 수정하지마. 결과 문장만 깔끔하게 보여줘. 이 문장들을 너가 잘 이해할 수 있게 프롬프트처럼 최종 수정된 문장만 보여줘.')
+        chain = (
+                prompt_2
+                | llm
+                | StrOutputParser()
+        )
+        #result.append(chain.invoke({"input": word[i], "sen": sen[i]}))
+        result += chain.invoke({"input": word[i], "sen": sen[i]})
+        result += " "
+    return result
+
+def openai_preview(sen, word):
+    llm = ChatOpenAI(temperature=0,  # 창의성 (0.0 ~ 2.0)
+                     max_tokens=2048,  # 최대 토큰수
+                     model_name='gpt-4o',  # 모델명
+                     )
+    result = ""
+    for i in range(len(word)):
+        prompt_2 = ChatPromptTemplate.from_template('"{input}" 이 명사를 "{sen}"이 문장의 __부분에 맞춤법을 맞춰서 넣어줘. 명사와 종결 어미는 수정하지마. 결과 문자열에서 프롬프트 내용은 빼고 결과 문장만 깔끔하게 보여줘. 이 문장들을 너가 잘 이해할 수 있게 프롬프트처럼 최종 수정된 문장만 보여줘.')
+        chain = (
+                prompt_2
+                | llm
+                | StrOutputParser()
+        )
+        # result.append(chain.invoke({"input": word[i], "sen": sen[i]}))
+        result += chain.invoke({"input": word[i], "sen": sen[i]})
+        result += " "
+    return result
+
+def gemini_pdf(pdf):
+    if pdf is "":
+        return None
+    # loader = OnlinePDFLoader(pdf)
+    loader = PyPDFLoader(pdf)
+    document = loader.load()
+    text_splitter = CharacterTextSplitter(chunk_size=256, chunk_overlap=50)
+    texts = text_splitter.split_documents(document)
+    embeddings = GoogleGenerativeAIEmbeddings(model="models/embedding-001")  # gemini의 임베딩 모델
+    docsearch = Chroma.from_documents(texts, embeddings)
+    retriever = docsearch.as_retriever()
+    return retriever
+
+def dgu_chatbot(question):
+    #llm = ChatGoogleGenerativeAI(model="gemini-pro")
+    llm = ChatOpenAI(temperature=0.0,  # 창의성 (0.0 ~ 2.0)
+                     max_tokens=2048,  # 최대 토큰수
+                     model_name='gpt-4o',  # 모델명
+                     )
+    # retriever = gemini_pdf("https://secure-project-dev-image.s3.ap-northeast-2.amazonaws.com/secure-project-using-image/computer.pdf")
+    user_prompt = ChatPromptTemplate.from_template(kindness_eng+"<Question>:{question}")
+    chain = (
+            user_prompt
+            | llm
+            | StrOutputParser()
+    )
+    return (chain.invoke(question))
